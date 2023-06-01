8e8f00a5582a7032217fe4cca14ab02b81f51732
Jeeten Mandal <jeetenmandal848@gmail.com>
2023-06-01 19:06:39 +0545

    mobile optimization blogs

--- src/app/blogs/earbuds-under-2500/page.jsx
+++ src/app/blogs/earbuds-under-2500/page.jsx
@@ -1,7 +1,98 @@
 import React from 'react'
+import Image from 'next/image'
+import Earbuds from '../../../../public/images/top view.jpg'
 
 export default function page() {
   return (
-    <div>earbuds under 2500</div>
+    <div className='blogPage'>
+      
+      <div className="blogMain">
+        <div className="blogLeft">
+        <h1>Best wireless earbuds under 2500 in Nepal</h1>
+      <p className="topDesc">
+        There are limited number of earbuds brands in Nepal. Among them very few are available under 2500. Most of the earbuds in this price range are unbranded and of low quality. But here are some earbuds which are of great quality in this price range.
+      </p>
+        <div className="blogPost">
+        <h2>1) GorkhaBuds Air </h2>
+       
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio laborum suscipit esse beatae error, distinctio nam possimus eveniet illum minus a soluta necessitatibus saepe sed perferendis similique magni reprehenderit. Iure doloribus necessitatibus eum aperiam obcaecati magnam saepe, sint aspernatur, placeat adipisci, aut odio velit maxime facilis exercitationem. Quos, perspiciatis placeat culpa officia maiores rem numquam possimus itaque fugit architecto ut vero maxime impedit facere ratione, repudiandae veritatis dicta eveniet.  <b>Rs 2499</b>
+        </p>
+        <div className="images">
+          <Image src={Earbuds} alt="our earbuds" />
+          <Image src={Earbuds} alt="our earbuds" />
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non autem repellendus commodi optio fugiat doloremque dolorem impedit ullam quibusdam, placeat libero beatae, sint unde ea error quo officia quia rerum fugit sapiente et explicabo ad. Illum minus, quia temporibus dolorem iste nemo vel officiis soluta harum corrupti unde molestias et maxime. A quas provident explicabo, rem officiis accusamus quasi cum quis, sapiente soluta aperiam fugit voluptas et adipisci ex nemo optio. Harum impedit cum sit perspiciatis sequi, quia quisquam.
+        </p>
+        <ul>
+          <li><b>Bluetooth version : </b> bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+          <li><b>Bluetooth version : </b>bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+        </ul>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laudantium neque veniam! Dolor veniam architecto consequatur! Et in quasi totam officia? Aspernatur inventore ipsum temporibus reiciendis repudiandae eveniet laboriosam soluta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, in maiores. Laborum obcaecati autem omnis accusantium magni harum cum earum dolorum delectus tenetur, incidunt illo explicabo quisquam, sint iure voluptatem.
+        </p>
+      </div>
+      <div className="blogPost">
+        <h2>2) GorkhaBuds Air </h2>
+       
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio laborum suscipit esse beatae error, distinctio nam possimus eveniet illum minus a soluta necessitatibus saepe sed perferendis similique magni reprehenderit. Iure doloribus necessitatibus eum aperiam obcaecati magnam saepe, sint aspernatur, placeat adipisci, aut odio velit maxime facilis exercitationem. Quos, perspiciatis placeat culpa officia maiores rem numquam possimus itaque fugit architecto ut vero maxime impedit facere ratione, repudiandae veritatis dicta eveniet.  <b>Rs 2299</b>
+        </p>
+        <div className="images">
+          <Image src={Earbuds} alt="our earbuds" />
+          <Image src={Earbuds} alt="our earbuds" />
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non autem repellendus commodi optio fugiat doloremque dolorem impedit ullam quibusdam, placeat libero beatae, sint unde ea error quo officia quia rerum fugit sapiente et explicabo ad. Illum minus, quia temporibus dolorem iste nemo vel officiis soluta harum corrupti unde molestias et maxime. A quas provident explicabo, rem officiis accusamus quasi cum quis, sapiente soluta aperiam fugit voluptas et adipisci ex nemo optio. Harum impedit cum sit perspiciatis sequi, quia quisquam.
+        </p>
+        <ul>
+          <li><b>Bluetooth version : </b> bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+          <li><b>Bluetooth version : </b>bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+        </ul>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laudantium neque veniam! Dolor veniam architecto consequatur! Et in quasi totam officia? Aspernatur inventore ipsum temporibus reiciendis repudiandae eveniet laboriosam soluta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, in maiores. Laborum obcaecati autem omnis accusantium magni harum cum earum dolorum delectus tenetur, incidunt illo explicabo quisquam, sint iure voluptatem.
+        </p>
+      </div>
+
+      <div className="blogPost">
+        <h2>3) GorkhaBuds Air </h2>
+       
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio laborum suscipit esse beatae error, distinctio nam possimus eveniet illum minus a soluta necessitatibus saepe sed perferendis similique magni reprehenderit. Iure doloribus necessitatibus eum aperiam obcaecati magnam saepe, sint aspernatur, placeat adipisci, aut odio velit maxime facilis exercitationem. Quos, perspiciatis placeat culpa officia maiores rem numquam possimus itaque fugit architecto ut vero maxime impedit facere ratione, repudiandae veritatis dicta eveniet.  <b>Rs 2399</b>
+        </p>
+        <div className="images">
+          <Image src={Earbuds} alt="our earbuds" />
+          <Image src={Earbuds} alt="our earbuds" />
+        </div>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non autem repellendus commodi optio fugiat doloremque dolorem impedit ullam quibusdam, placeat libero beatae, sint unde ea error quo officia quia rerum fugit sapiente et explicabo ad. Illum minus, quia temporibus dolorem iste nemo vel officiis soluta harum corrupti unde molestias et maxime. A quas provident explicabo, rem officiis accusamus quasi cum quis, sapiente soluta aperiam fugit voluptas et adipisci ex nemo optio. Harum impedit cum sit perspiciatis sequi, quia quisquam.
+        </p>
+        <ul>
+          <li><b>Bluetooth version : </b> bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+          <li><b>Bluetooth version : </b>bluetooth version 5.3</li>
+          <li><b>Bluetooth version : </b>delay less than 30 ms</li>
+          <li><b>Bluetooth version : </b>sturdy build quality</li>
+        </ul>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laudantium neque veniam! Dolor veniam architecto consequatur! Et in quasi totam officia? Aspernatur inventore ipsum temporibus reiciendis repudiandae eveniet laboriosam soluta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, in maiores. Laborum obcaecati autem omnis accusantium magni harum cum earum dolorum delectus tenetur, incidunt illo explicabo quisquam, sint iure voluptatem.
+        </p>
+      </div>
+
+        </div>
+        <div className="blogRight"></div>
+      </div>
+      
+    </div>
   )
 }
